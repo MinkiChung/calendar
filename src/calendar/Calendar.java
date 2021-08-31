8a7440c126b80d8429cc0482c81a660ca43f307c
정민기 <jmk05099@ggmail.com>
2021-08-31 21:54:50 +0900

    commit

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,6 +1,8 @@
 package calendar;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calendar {
@@ -11,14 +13,20 @@ public class Calendar {
         return Months[month - 1];
     }
 
-    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calendar cal = new Calendar();
-        System.out.println("달을 입력하세요.");
-        int month = scanner.nextInt();
 
-        System.out.printf("%d월은 %d일까지 있습니다.\n", month,  cal.getMaxDaysOfMonth(month));
+        System.out.println("반복횟수를 입력하세요");
+        int repeat = scanner.nextInt();
+
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("달을 입력하세요");
+            int month = scanner.nextInt();
+            System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+        }
+
+        System.out.println("Bye~");
         scanner.close();
     }
 }
