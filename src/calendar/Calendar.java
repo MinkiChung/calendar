d3bbffb6dc6042b13a9649c26122fe92f943015e
정민기 <jmk05099@ggmail.com>
2021-08-31 18:38:26 +0900

    commit

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,14 +1,18 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String args[]) {
-        System.out.println("일 월 화 수 목 금 토\n" +
-                "--------------------\n" +
-                " 1  2  3  4  5  6  7\n" +
-                " 8  9 10 11 12 13 14\n" +
-                "15 16 17 18 19 20 21\n" +
-                "22 23 24 25 26 27 28");
+        System.out.println("두 수를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        String numbers = scanner.nextLine();
+        String[] splitedValue = numbers.split(" ");
+        int first = Integer.parseInt(splitedValue[0]);
+        int second = Integer.parseInt(splitedValue[1]);
 
+        System.out.printf("%d와 %d의 합은 %d입니다.", first, second, first+second);
 
+        scanner.close();
     }
 }
