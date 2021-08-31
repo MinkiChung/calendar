6e29cd1dbe8f89738c836e7552ae21b053991033
정민기 <jmk05099@ggmail.com>
2021-08-31 19:17:30 +0900

    commit

--- src/calendar/Sum.java
+++ src/calendar/Sum.java
@@ -2,7 +2,7 @@ package calendar;
 
 import java.util.Scanner;
 
-public class Calendar {
+public class Sum {
     public static void main(String args[]) {
         System.out.println("두 수를 입력하세요.");
         Scanner scanner = new Scanner(System.in);
